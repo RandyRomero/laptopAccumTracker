0fde602702b51860bc7de80a412d83369581d3f2
RandyRomero <a.ya.miheev@gmail.com>
2017-12-18 09:00:44 +0300

    Update readme; fix some styling in accumTimer.py; update .gitignore

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# laptopAccumTracker
-Small script that infinetely checks battery status of laptop . 
-It prints out and writes to logFile percentage of charge and current time every 120 seconds or every N seconds you want it to. 
-Initially it checks if there is 'psutil' module on PC. if not, it will be installed. 
-It can also show warning message if laptop is not unplugged. 
-As a new feature it can opn Google Chrome and some page there - for battery benchmarking purpose. 
+# laptop_accum_tracker
+
+Small script that checks battery status of a laptop infinitely. Tested on Windows 10.
+It prints out and writes to logFile the percentage of charge and current time every 120 seconds or every N seconds you want it to. 
+Initially it checks if there is 'psutil' module on your PC. If not, it will be installed. 
+Script can also show warning message if laptop is not unplugged. 
+As a new feature it can open Google Chrome and some page there - for battery benchmarking purpose. 
--- accumTimer.py
+++ accumTimer.py
@@ -1,61 +1,68 @@
-#! python3
+#!python3
 
-#Small script that infinetely checks battery status of laptop until its become lower than 11%. It prints out and writes to logFile percentage of charge and current time every 120 seconds or every N seconds you want it to. Initially it checks if there is 'psutil' module on PC. if not, it will be installed. 
+# Small script that infinitely checks battery status of a laptop until its become lower than 11%.
+# It prints out and writes to logFile percentage of charge and current time every 120 seconds
+# or every N seconds you want it to.
+# Initially it checks if there is 'psutil' module on PC. if not, it will be installed.
 
-import os, pip, time, logging, subprocess, sys
+import pip
+import time
+import logging
+import subprocess
+import sys
 
-#checks if module 'psutil' has already been installed. If not - it's gonna be installed
+# Checks if module 'psutil' has already been installed. If not - it's gonna be installed
 try:
-	__import__('psutil')
+    __import__('psutil')
 except ImportError:
-	pip.main(['install', 'psutil'])
+    pip.main(['install', 'psutil'])
 
 import psutil
 
 logging.basicConfig(
-	format = "%(levelname) -1s %(asctime)s line %(lineno)s: %(message)s",
-	level = logging.DEBUG
-	)
+    format="%(levelname) -1s %(asctime)s line %(lineno)s: %(message)s",
+    level=logging.DEBUG
+    )
+
 
 def prlog(message):
-	print(message)
-	logFile.write(message + '\n')
+    print(message)
+    logFile.write(message + '\n')
+
 
 while True:
-	startBrowser = (input('Hi. Do you want to start browser benchmark? (y/n): ')).lower()
-	if startBrowser == 'y':
-		#run 'browserBench' simultaneously with 'accumTimer'
-		subprocess.Popen([sys.executable, '.\\browserBench.py'])
-		break
-	elif startBrowser == 'n':
-		print('Ok, only timer is gonna work.')
-		break
-	else:
-		print('Input error. You should type in only "y" on "n".')	
+    startBrowser = (input('Hi. Do you want to start browser benchmark? (y/n): ')).lower()
+    if startBrowser == 'y':
+        # Run 'browserBench' simultaneously with 'accumTimer'
+        subprocess.Popen([sys.executable, '.\\browserBench.py'])
+        break
+    elif startBrowser == 'n':
+        print('Ok, only timer is gonna work.')
+        break
+    else:
+        print('Input error. You should type in only "y" on "n".')
 
-
-#get time to add to name of log file
+# Get time to add to name of log file
 timestr = time.strftime('%Y-%m-%d__%Hh%Mm%Ss')
 
-#get battery status
+# Get battery status
 battery = psutil.sensors_battery()
 percent = battery.percent
 
-# check and write in file status of battery every N sec until there is only 10 percent left
 totalTime = 0
+# How often to check battery status
 timer = 120
 
 while True:
-	battery = psutil.sensors_battery()
-	percent = battery.percent
-	timeNow = time.strftime('%Hh%Mm%Ss')
-	#open log file in write mode with current time in its name
-	logFile = open('BatteryStatus ' + timestr + '.txt', 'a')
-	if battery.power_plugged == True:
-		prlog('\nWARNING! You forget to unplug te laptop.')
-	prlog(timeNow + ': battery level is ' + str(percent) + '%.')
-	prlog('Script has already been working for ' + str('%0.0f' % (totalTime/60) + ' minute(s).\n'))
-	logFile.close()
-	#timer is here, in seconds
-	time.sleep(timer)
-	totalTime = totalTime + timer
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    timeNow = time.strftime('%Hh%Mm%Ss')
+    # Open log file in write mode with current time in its name
+    logFile = open('BatteryStatus ' + timestr + '.txt', 'a')
+    if battery.power_plugged:
+        prlog('\nWARNING! You forget to unplug te laptop.')
+    prlog(timeNow + ': battery level is ' + str(percent) + '%.')
+    prlog('Script has already been working for ' + str('%0.0f' % (totalTime/60) + ' minute(s).\n'))
+    logFile.close()
+    time.sleep(timer)
+    totalTime = totalTime + timer
