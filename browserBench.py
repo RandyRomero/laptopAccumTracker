ad39d83570332efc3d964d0b7bb09e577f74705a
mozjiang <jms292569413@sina.com>
2017-12-31 00:05:50 +0800

    get url in order => get it randomly

--- browserBench.py
+++ browserBench.py
@@ -3,6 +3,7 @@
 '''Script that infinitely launch some URL in crhome'''
 
 import pip, time
+import random
 
 try:
 	from selenium import webdriver
@@ -13,14 +14,15 @@ from selenium import webdriver
 
 browser = webdriver.Chrome(executable_path=r".\\chromedriver.exe")
 
+urlList = [
+	'https://www.computeruniverse.ru/',
+	'https://unsplash.com/',
+	'https://3dnews.ru/',
+	'http://www.ferra.ru/',
+] 
 toggleTimer = 25
 
 while True:
-	browser.get('https://www.computeruniverse.ru/')
+	browser.get(random.choice(urlList))
 	time.sleep(toggleTimer)
-	browser.get('https://unsplash.com/')
-	time.sleep(toggleTimer)
-	browser.get('https://3dnews.ru/')
-	time.sleep(toggleTimer)
-	browser.get('http://www.ferra.ru/')
-	time.sleep(toggleTimer)
+	
